1551c09483de11b3ce63a4c8be633855800478c9
Isaac Gerard <IsaacBathSpa@gmail.com>
2024-03-30 23:15:29 +0400

    Soundboard.js

--- Chapter-5-Embedded Content/Exercises/Audio Sampler/Soundboard/Soundboard.js
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/Soundboard/Soundboard.js
@@ -13,6 +13,32 @@ sounds.forEach(sound => {
     // To add the corresponding texts inside each button.
     btn.innerText = sound;
     
+
+// The following lines is to display the length of the sample in seconds underneath the sample title.
+// To retrieve the audio elements by their IDs.
+const sampleaudio = document.getElementById(sound);
+
+// This line triggers a function that would be executed once the metadata is available.
+sampleaudio.onloadedmetadata = function() {
+
+// I created this line to calculate the length of the samples in seconds and rounding them off.    
+const samplelength = Math.round(sampleaudio.duration);
+      
+    // I created this line to create a span element that would hold the duration texts.
+    const lengthofsample = document.createElement('span');
+
+    // This line sets the text content with duration
+    lengthofsample.innerText = `(${samplelength} second/s)`;
+      
+    // The following lines is to place the duration texts underneath the button text.
+    // This is used to add a line break. 
+    btn.appendChild(document.createElement('br'));
+
+    // This line is to append the durationSpan.
+    btn.appendChild(lengthofsample);
+  };
+
+
     // I used an event listener on the button to handle any instances where the user clicks it.
     btn.addEventListener('click', () => {
 
